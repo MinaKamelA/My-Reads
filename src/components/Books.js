f4e57513975b714e6a3b8f74d9c01ecffe756bf3
MinaKamelA <developmentmina@gmail.com>
2022-06-23 18:30:51 +0200

    add Link to book details on books

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import ShelfChanger from './ShelfChanger';
 
 const Books = ({ books, onShelfChange }) => {
     return (
         books.map((book) => {
+            const bookCover = book.imageLinks ? book.imageLinks.thumbnail : "https://cdn-d8.nypl.org/s3fs-public/blogs/J5LVHEL.jpg";
             return (
                 <li key={book.id}>
                     <div className="book">
@@ -14,13 +16,15 @@ const Books = ({ books, onShelfChange }) => {
                                     width: 128,
                                     height: 193,
                                     backgroundImage:
-                                        `url(${book.imageLinks.thumbnail})`,
+                                        `url(${bookCover})`,
+                                    backgroundSize: "contain",
                                 }}
                             ></div>
                             <ShelfChanger onShelfChange={onShelfChange} book={book} />
                         </div>
                         <div className="book-title">{book.title}</div>
                         <div className="book-authors">{book.authors}</div>
+                        <Link to="/book" state={{ id: book.id }}>Show more</Link>
                     </div>
                 </li>
             )
